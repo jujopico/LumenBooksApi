faba1b7c277b28c12f94cecc1cb920fabb857526
jujopico <sujunqt@gmail.com>
2020-01-22 14:23:14 -0500

    create book model

--- app/Book.php
+++ app/Book.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 
-class User extends Model implements AuthenticatableContract, AuthorizableContract
+class Book extends Model 
 {
     use Authenticatable, Authorizable;
 
@@ -18,15 +18,9 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      * @var array
      */
     protected $fillable = [
-        'name', 'email',
-    ];
-
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
+        'title',
+        'description',
+        'price',
+        'author_id',
     ];
 }
